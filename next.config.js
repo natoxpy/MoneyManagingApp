9dc1394876a13706a3a3b49cfab08b3f32fd8c34
natoxpy <>
2022-03-28 13:43:46 -0400

    Save URI safetly

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
+const { uri } = require('./env.json');
 
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        uri: ''
+        uri: uri
     }
 }
 
