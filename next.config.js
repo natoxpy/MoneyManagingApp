d3fae436a3e1f643d771c5a1f71a82ccdc1820d2
natoxpy <>
2022-03-28 14:26:19 -0400

    Changes to URI env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const { URI } = require('./env.json');
+const fileEnv = require('./env.json');
 
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        uri: URI
+        uri: fileEnv.URI || process.env.URI
     }
 }
 
